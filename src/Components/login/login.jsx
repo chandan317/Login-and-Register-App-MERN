8a81f53665dd5db8684d2aea5a6481ad96d93ce7
chandantm317 <chandantmchandu31@gmail.com>
2022-12-05 13:20:31 +0530

    first commit

---
+++ src/Components/login/login.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./login.css"
+
+const Login =()=>{
+
+    return(
+        <div className=" login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Enter your Email"  />
+            <input type="password"  placeholder="Enter your password"/>
+            <div className="button">Login</div>
+            <div>or</div>
+            <div className="button">Register</div>
+            
+        </div>
+    )
+}
+export default Login
